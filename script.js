588a3dd89fb085c210c2cfac111a849c371ab41d
juliancallegari1 <juliancallegari7@gmail.com>
2025-03-13 18:24:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generateNickname() {
-    const adjectives = ["Boss", "Piss", "Anal", "Cummy", "Veiny", "Squirting", "Shit"];
-    const nouns = ["Sodomizer", "Baby", "Goblin", "Samurai", "Butt", "Penis", "Whore", "Tits"];
+    const adjectives = ["Boss", "Piss", "Anal", "Cummy", "Veiny", "Squirting", "Shit", "Mega", "Gay", "Mister", "Professor", "Master"];
+    const nouns = ["Sodomizer", "Baby", "Goblin", "Samurai", "Butt", "Penis", "Whore", "Tits", "Tree", "Journalist", "Little Engineer", "Cartographer"];
 
     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
     const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
